6d6660022cab75b80f3d76390e889177c55bb91c
Noela-Murugi <murugin254@gmail.com>
2022-10-31 00:34:51 +0300

    update class Magazine

--- solution.rb
+++ solution.rb
@@ -16,3 +16,36 @@ class Article
       @@all
   end
 end
+
+# MAGAZINE
+require_relative "./article"
+ class Magazine
+   attr_accessor :name, :category
+   @@all = []
+
+   def initialize(name, category)
+     @name = name
+     @category = category
+     @@all << self
+   end
+
+   def self.all
+     @@all
+   end
+   def contributors
+    Article.all.filter {|article| article.magazine == self}.map {|article| article.author}
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    Article.all.filter {|article| article.magazine == self}.map {|article| article.title}
+  end
+
+  def contributing_authors
+    Article.all.filter {|article| article.magazine == self}.map{|article| article.author}.uniq
+    .filter {|author|  all_magazine_authors.count(author) > 2}
+  end
+end
