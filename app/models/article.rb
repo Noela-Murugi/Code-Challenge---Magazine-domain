47ea22a6200bff55ffbcd08312aab478c7067985
Noela-Murugi <murugin254@gmail.com>
2022-10-30 23:51:29 +0300

    completed the challenge

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,15 @@
 class Article
+  attr_reader :author, :magazine, :title
+  @@all = []
 
+  def initialize(author, name, category, title)
+      @author = Author.new(author)
+      @magazine =Magazine.new(name,category)
+      @title = title
+      @@all << self
+  end
 
+  def self.all
+      @@all
+  end
 end
-
