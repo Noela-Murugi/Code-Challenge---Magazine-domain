3260c869963302ff6ed636b1a6c914c046b50c70
Noela-Murugi <murugin254@gmail.com>
2022-10-30 23:54:48 +0300

    update class Article

--- solution.rb
+++ solution.rb
@@ -1,2 +1,18 @@
 # Please copy/paste all three classes into this file to submit your solution!
 
+# ARTICLE
+class Article
+  attr_reader :author, :magazine, :title
+  @@all = []
+
+  def initialize(author, name, category, title)
+      @author = Author.new(author)
+      @magazine =Magazine.new(name,category)
+      @title = title
+      @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+end
