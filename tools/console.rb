13ea34c6b86e1dc7a7523eb85a4511ba603649cb
Noela-Murugi <murugin254@gmail.com>
2022-10-30 23:11:52 +0300

    completed the challenge

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,20 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+# AUTHOR
+newauthor1 = Author.new("Noela")
+newauthor2 = Author.new("Bianca")
+newauthor3 = Author.new("Glen")
+
+# MAGAZINE
+newmagazine1 = Magazine.new("Sporting News", "sports")
+newmagazine2 = Magazine.new("ABC News", "entertainment")
+newmagazine3 = Magazine.new("Entertainment Weekly", "entertainment")
+
+
+# ARTICLE
+newarticle1 = Article.new("Noela", "Sporting News", "sports", "Ronaldo Ballon d'or Wins")
+newarticle2 = Article.new("Bianca", "ABC News", "entertainment", "Black Adam takes top spot at box office again")
 
 
 
