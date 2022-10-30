eeabbd3672b0fbb98a088eccc2bcb4b2503b5855
Noela-Murugi <murugin254@gmail.com>
2022-10-31 01:06:24 +0300

    completed the challenge

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,32 @@
+require_relative "./article.rb"
 class Author
-  attr_accessor :name
-
-
+  attr_reader :name
+  @magazine = []
   def initialize(name)
     @name = name
-    
   end
 
+  def articles
+    Article.all.select do |article|article.author.name == self.name
+    end
+  end
 
+  def add_article(title, name, category)
+    Article.new(title, self, name, category)
+  end
+
+  def magazines
+    magazine_self.uniq
+  end
+
+  private
+  def magazine_self
+    self.articles.map do
+      |article|article.magazine
+    end
+  end
+
+  def topic_areas
+    @magazine.map {|magazine|magazine.category}.uniq
+  end
 end
--- solution.rb
+++ solution.rb
@@ -49,3 +49,37 @@ require_relative "./article"
     .filter {|author|  all_magazine_authors.count(author) > 2}
   end
 end
+
+# AUTHOR
+require_relative "./article.rb"
+class Author
+  attr_reader :name
+  @magazine = []
+  def initialize(name)
+    @name = name
+  end
+
+  def articles
+    Article.all.select do |article|article.author.name == self.name
+    end
+  end
+
+  def add_article(title, name, category)
+    Article.new(title, self, name, category)
+  end
+
+  def magazines
+    magazine_self.uniq
+  end
+
+  private
+  def magazine_self
+    self.articles.map do
+      |article|article.magazine
+    end
+  end
+
+  def topic_areas
+    @magazine.map {|magazine|magazine.category}.uniq
+  end
+end
